3f29f0f5ba8d03e03a7db61ad4844a1014372ebe
misikdmitriy <misik.dmitriy@gmail.com>
2018-11-17 15:16:09 +0200

    add docker

--- SportChallenge/Program.cs
+++ SportChallenge/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace SportChallenge
 {
@@ -19,6 +13,7 @@ namespace SportChallenge
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls(new ConfigurationBuilder().AddCommandLine(args).Build()["urls"]);
     }
 }
